a2561102a81ce46fe9ca89cf85f8f6892040be3f
Unknown <six.fingers.1988@gmail.com>
2019-07-20 11:46:38 +0100

    Logged user Global Scope

---
+++ app/Scopes/LoggedUserScope.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class LoggedUserScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $user_id = auth('api')->user()->id;
+
+        $builder->where('user_id', $user_id);
+    }
+}
